3234056d9457a849ab85165813cf32dd9de18608
RIck Dalley <dalley.rick@gmail.com>
2017-09-21 17:29:32 -0400

    Initial Commit

---
+++ CMMahjongSpike/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CMMahjongSpike
+//
+//  Created by Richard Dalley on 2017-09-21.
+//  Copyright © 2017 CodeMojo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
